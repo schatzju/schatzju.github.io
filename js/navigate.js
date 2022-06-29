1949cce9be24c564589cd868549210fca20d7aa5
Jule Schatz <juleschatz@Jules-MacBook-Air.local>
2022-06-29 01:05:06 -0500

    changes

--- js/navigate.js
+++ js/navigate.js
@@ -6,7 +6,30 @@ document.getElementById('research_button').onclick = function(event) {
    location.href = 'research.html';
 }
 
-document.getElementById('blog_button').onclick = function(event) {
-   location.href = 'https://paper.dropbox.com/doc/Blog--AUrycgOz0Kb2xr~Y~ZYUiLRlAQ-lMaBjTlFcRizi4zllV6Qd';
+document.getElementById('vlog_button').onclick = function(event) {
+   location.href = 'https://www.youtube.com/watch?v=lPHhQPCE8A4&list=PLQKdcWBkHsUsV85teRjXJ9hmfJ0zBt2JA';
 }
 
+document.getElementById('teaching_button').onclick = function(event) {
+   location.href = 'teaching.html'
+}
+
+        var images = [
+            "img/personal/grad_1.JPG",
+            "img/personal/pers5.jpeg",             
+            "img/personal/pers6.jpg", 
+            "img/personal/pers10.jpg",
+            "img/personal/pers3.jpg", 
+            "img/personal/pers7.jpeg",
+            "img/personal/pers2.jpeg", 
+            "img/personal/pers8.jpg", 
+        ];
+
+        var imageIndex = 0;
+
+document.getElementById('front_image').onclick = function(event) {
+            imageIndex = imageIndex + 1;
+            imageIndex = (imageIndex) % (images.length);    
+
+    $("#front_image").attr('src', images[imageIndex]);
+}
